da51875a076ca7c45f0ce6c62b2663c57e53387e
Don Mallory <dmm333@gmail.com>
2020-05-26 16:21:57 -0700

    refactored json by url

--- db/migrate/20200526213610_add_id_desc_type_link_st_live_to_events.rb
+++ db/migrate/20200526213610_add_id_desc_type_link_st_live_to_events.rb
@@ -1,10 +1,10 @@
 class AddIdDescTypeLinkStLiveToEvents < ActiveRecord::Migration[6.0]
   def change
-    add_column :events, :id, :integer
+    add_column :events, :event_id, :integer
     add_column :events, :description, :string
     add_column :events, :type, :string
     add_column :events, :link, :string
-    add_column :events, :startTime, :datetime
+    add_column :events, :start_time, :datetime
     add_column :events, :live, :boolean
   end
 end
--- lib/json_by_url.rb
+++ lib/json_by_url.rb
@@ -1,24 +1,22 @@
 class JSONByURL
     attr_reader :url, :logger
+    attr_accessor :result, :clean, :code, :json
 
     def initialize(url, logger = false)
         @url = url
         @logger = logger
-    end
-
-    def snag
-        result = {clean: false}
+    
+        @result = {clean: false}
         
         puts "GETting JSON: #{url}" if @logger
         begin
           response = RestClient.get(url)
-          result.code = response.code
-          result.json = JSON.parse(response)
+          @code = response.code
+          @json = JSON.parse(response)
         rescue RestClient::BadRequest => e
-          result.code = e.http_code
+          @code = e.http_code
         else #check for 2xx also?
-          result.clean = true
+          @clean = true
         end
-        result
     end
 end
