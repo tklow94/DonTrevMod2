067ebfa993974c2cf12a9171a8734515da049425
Don Mallory <dmm333@gmail.com>
2020-05-28 12:15:19 -0700

    fixed issue with no-JS for updating sub_name select

--- app/controllers/competitors_controller.rb
+++ app/controllers/competitors_controller.rb
@@ -8,6 +8,8 @@ class CompetitorsController < ApplicationController
         @subs = Sport.get_names("Comp", "sub_name", "sport_name = '#{@selected[0]}'")
         @selected[1] = params[:sub_name] || @subs.sample
         
+        @selected[1] = @subs.sample unless @subs.include?(@selected[1])  #useful catch since we're not using JS to repopulate sub_name select -> force to use new param
+        
         @comp = Competitor.all.where(sport_name: @selected[0], sub_name: @selected[1])
     end
 
