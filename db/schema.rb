5d9827f98d31bacaed390b9521e44c6750091e49
Don Mallory <dmm333@gmail.com>
2020-05-29 03:15:01 -0700

    end of night

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_220118) do
+ActiveRecord::Schema.define(version: 2020_05_29_101216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,12 +77,6 @@ ActiveRecord::Schema.define(version: 2020_05_28_220118) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_watch_lists_on_user_id"
-  end
-
-  create_table "welcomes", force: :cascade do |t|
-    t.string "index"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "watch_lists", "users"
