a46ee0449009267f3d72ddc41e466ad53aeef08b
Don Mallory <dmm333@gmail.com>
2020-05-29 13:23:27 -0700

    Project Complete

--- app/models/competitor.rb
+++ app/models/competitor.rb
@@ -2,4 +2,12 @@ class Competitor < ApplicationRecord
     has_many :involvements, inverse_of: :competitor
     has_many :events, through: :involvements
 
+
+
+    def assoc_wagers
+        events.inject(0) do |sum, e|
+            sum += e.markets.count
+            sum
+        end
+    end
 end
